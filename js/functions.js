813348232c600553f01b63cfe91a6e6b1ddba64b
Ольга Разживина <razjivina.olia@gmail.com>
2023-02-25 13:11:02 +0300

    Приводит все функции к стрелочным

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,37 @@
 //Функция для проверки длины строки
 
-function inspectStringLength (stringValue, maxLength) {
-  return String(stringValue).length <= maxLength;
-}
+const inspectStringLength = (stringValue, maxLength) => String(stringValue).length <= maxLength;
 inspectStringLength ('Проверяемая строка', 20);
 
 //Функция для проверки, является ли строка палиндромом
 
-function isPalindrom (stringValue) {
+const isPalindrom = (stringValue) => {
   const convertedString = stringValue.toLowerCase().replaceAll(' ', '');
   let reverseString = '';
   for (let i = convertedString.length - 1; i >= 0; i--) {
     reverseString += convertedString.at(i);
   }
   return convertedString === reverseString;
-}
+};
 isPalindrom('Лёша на полке клопа нашёл');
 
 
 //Функция, которая извлекает из строки число
 
-function extractNumber (stringValue) {
-  let upshot = '';
+const extractNumber = (stringValue) => {
+  let result = '';
   for (let i = 0; i < stringValue.length; i++) {
     if (!Number.isNaN(parseInt(stringValue.at(i), 10))) {
-      upshot += stringValue.at(i);
+      result += stringValue.at(i);
     }
   }
-  return parseInt(upshot,10);
-}
+  return parseInt(result,10);
+};
 extractNumber('1 кефир, 0.5 батона');
 
 //Функция, которая добавляет строке символы
 
-function addStringSymbol (string, minLength, symbol) {
+const addStringSymbol = (string, minLength, symbol) => {
   while (string.length < minLength) {
     if (symbol.length <= minLength - string.length) {
       string = symbol + string;
@@ -43,5 +41,5 @@ function addStringSymbol (string, minLength, symbol) {
     }
   }
   return string;
-}
+};
 addStringSymbol('1', 4, '0');
