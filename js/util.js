3adb76cc6798167cf313a83aafa86f8980cfe934
Ольга Разживина <razjivina.olia@gmail.com>
2023-03-30 16:39:02 +0300

    Добавляет показ сообщений об успехе/ошибке. Создает показ alert. Удаляет временные функции для разработки

--- js/util.js
+++ js/util.js
@@ -1,31 +1,108 @@
-//Функция возвращает случайное целое положительное число
+import {onDocumentKeydown, closePopupForm} from './popup-form.js';
 
-export const getRandomPositiveInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const ALERT_SHOW_TIME = 5000;
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '20px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+
 };
 
-//Функция возвращает случайный элемент массива
+const errorMessageElement = document.querySelector('#error').content;
+const successMessageElement = document.querySelector('#success').content;
 
-export const getRandomArrayElements = (elements) => elements [getRandomPositiveInteger(0, elements.length - 1)];
 
+const createMessageError = () => {
+  const errorClone = errorMessageElement.querySelector('section').cloneNode(true);
+  document.body.insertAdjacentElement('beforeend', errorClone);
 
-// Функция для генерации уникального (неповторяющегося) числа из указанного дипазона
-export const getUniqueNumberFromRange = (min, max) => {
-  const previousValues = [];
-  return function () {
-    let currentValue = getRandomPositiveInteger(min, max);
-    if (previousValues.length > (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomPositiveInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
+  document.removeEventListener('keydown', onDocumentKeydown);
+  document.addEventListener('keydown', onErrorKeydown);
+
+  const removeErrorMessage = () => {
+    errorClone.remove();
+    document.removeEventListener('keydown', onErrorKeydown);
+    document.addEventListener('keydown', onDocumentKeydown);
   };
+
+  const removeErrorOutside = (evt) => {
+    if (!evt.target.closest('.error__inner')) {
+      removeErrorMessage();
+      document.removeEventListener('click', removeErrorOutside);
+    }
+  };
+
+  function onErrorKeydown(evt) {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      errorClone.remove();
+      document.removeEventListener('keydown', onErrorKeydown);
+      document.addEventListener('keydown', onDocumentKeydown);
+      document.removeEventListener('click', removeErrorOutside);
+    }
+  }
+  errorClone.querySelector('.error__button').addEventListener('click', () => {
+    removeErrorMessage();
+    document.removeEventListener('click', removeErrorOutside);
+  });
+
+  document.addEventListener('click', removeErrorOutside);
 };
 
-export const isEscapeKey = (evt) => evt.key === 'Escape';
+const createMessageSuccess = () => {
+  const successClone = successMessageElement.querySelector('section').cloneNode(true);
+  document.body.insertAdjacentElement('beforeend', successClone);
+
+  const removeSuccessMessage = () => {
+    closePopupForm();
+    successClone.remove();
+    document.removeEventListener('keydown', onSuccessKeydown);
+  };
+
+
+  const removeSuccessOutside = (evt) => {
+    if (!evt.target.closest('.success__inner')) {
+      removeSuccessMessage();
+      document.removeEventListener('click', removeSuccessOutside);
+    }
+  };
+
+  function onSuccessKeydown(evt) {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      successClone.remove();
+      document.removeEventListener('keydown', onSuccessKeydown);
+      document.removeEventListener('click', removeSuccessOutside);
+    }
+  }
+
+  document.addEventListener('keydown',onSuccessKeydown);
+
+  successClone.querySelector('.success__button').addEventListener('click', () => {
+    removeSuccessMessage();
+    document.removeEventListener('click', removeSuccessOutside);
+  });
+
+  document.addEventListener('click', removeSuccessOutside);
+};
+
+export {isEscapeKey, showAlert, createMessageSuccess, createMessageError};
