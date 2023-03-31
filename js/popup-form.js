a659db03b18f02283b112415751c3ea113d77fa8
Ольга Разживина <razjivina.olia@gmail.com>
2023-03-31 16:26:36 +0300

    Добавляет загрузку выбранного пользователем изображения

--- js/popup-form.js
+++ js/popup-form.js
@@ -2,13 +2,17 @@ import {isEscapeKey} from './util.js';
 import {resetValueScale} from './scale.js';
 import {resetEffects} from './effect.js';
 
+const FORMATS_FILE = ['jpeg', 'png', 'jpg'];
+
 const bodyElement = document.querySelector('body');
-const imgInputElement = document.querySelector('.img-upload__input');
-const imgUploadPopupElement = document.querySelector('.img-upload__overlay');
+const inputPictureElement = document.querySelector('.img-upload__input');
+const uploadPopupPictureElement = document.querySelector('.img-upload__overlay');
 const cancelButtonElement = document.querySelector('.img-upload__cancel');
 const hashtagFieldElement = document.querySelector('.text__hashtags');
 const commentFieldElement = document.querySelector('.text__description');
 const formElement = document.querySelector('.img-upload__form');
+const previewPictureElement = document.querySelector('.img-upload__preview');
+const miniaturePreviewPicturesElement = document.querySelectorAll('.effects__preview');
 
 
 const isFieldFocused = () =>
@@ -22,7 +26,7 @@ const onDocumentKeydown = (evt) => {
 };
 
 function openPopupForm() {
-  imgUploadPopupElement.classList.remove('hidden');
+  uploadPopupPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 }
@@ -31,11 +35,10 @@ function closePopupForm() {
   formElement.reset();
   resetValueScale();
   resetEffects();
-  imgUploadPopupElement.classList.add('hidden');
+  uploadPopupPictureElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 }
-
 
 const onFileUploadClick = () => {
   openPopupForm();
@@ -45,8 +48,26 @@ const onCloseButtonClick = () => {
   closePopupForm();
 };
 
+const displayPicture = (picture) => {
+  const img = URL.createObjectURL(picture);
+  previewPictureElement.children[0].src = img;
+  miniaturePreviewPicturesElement.forEach((child) => {
+    child.style.backgroundImage = `url(${img})`;
+  });
+};
 
-imgInputElement.addEventListener('change', onFileUploadClick);
+inputPictureElement.addEventListener('change',() => {
+  onFileUploadClick();
+  const file = inputPictureElement.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FORMATS_FILE.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    displayPicture(file);
+  }
+});
+
 cancelButtonElement.addEventListener('click', onCloseButtonClick);
 
 
